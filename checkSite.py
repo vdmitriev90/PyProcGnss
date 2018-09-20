7b0d3db0f842e2adf7c2f114f6b3301738772171
Vasily Dmitriev <vm.dmitriev90@gmail.com>
2018-09-20 11:04:18 +0300

    optimization in 'checkSite' script

--- checkSite.py
+++ checkSite.py
@@ -3,13 +3,15 @@ import datetime as dt
 import sqlite3
 import matplotlib.pyplot as plt
 import argparse
+import time
 
 def check():
+    #tstart = time.localtime(time.time())
     parser = argparse.ArgumentParser()
     parser.add_argument('--site', help='site to be processed', required=True)
     #parser.add_argument("-v",'--vvv', help='test argument', required=True)
     args = parser.parse_args()
-    site = args.site
+    site = args.site.upper()
     
     print (site)
     conn = sqlite3.connect("MOS-NETS-Stat.db") 
@@ -17,24 +19,33 @@ def check():
     #stmt = "SELECT DISTINCT SITE FROM DATA;"
     stmt = "SELECT TIME, size FROM DATA WHERE SITE = '{0}' ORDER BY Time;".format(site)
     cursor.execute(stmt)
-    print (cursor) 
+    #print (cursor) 
     #for res in cursor.fetchall():
     res =  cursor.fetchall()
     #for it in res: print (it)
 
     times = list(map( lambda x: dt.datetime.strptime(x[0],'%Y-%m-%d %H:%M:%S'), res))
-    t0 =times[0]
+    
+    t0 = times[0]
+    print(t0)
+
     te = times[-1]
+    print(te)
+
+    timesSet = frozenset(times)
+
     args = list()
     values = list()
+    print("loop started")
     while t0<te:
         args.append(t0)
-        if t0 in times:
+        if t0 in timesSet:
             values.append(1)
         else:
             values.append(0)
         t0 +=dt.timedelta(hours=1)
 
+    print("plot started")
     plt.plot(args,values,'ro')
     plt.show()
     conn.close()
